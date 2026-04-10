255a6b45b1643d7d5ed3552ee9c6fdc32044c0ef
Shane <6071159+smashedr@users.noreply.github.com>
2026-04-10 12:01:08 -0700

    Updates and Test

--- vite.config.ts
+++ vite.config.ts
@@ -2,18 +2,15 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
   base: '/vue-test/',
-  plugins: [vue(), vueDevTools()],
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-  server: {
-    allowedHosts: true,
-  },
+  server: { allowedHosts: true },
 })
